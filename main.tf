4284b9f15f5bfdb62a691f0eef83c2123cc457c4
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-08-03 12:03:46 +0530

    terraform

--- main.tf
+++ main.tf
@@ -12,5 +12,26 @@ variable "params"{
    { name = "roboshop.frontend.cart_url", value = "http://cart.devops2406.xyz8080/", type = "String" },
    { name = "roboshop.frontend.payment_url", value = "http://payment.devops2406.xyz:8080/", type = "String" },
    { name = "roboshop.frontend.shipping_url", value = "http://shipping.devops2406.xyz:8080/", type = "String" },
-]
+   { name = "roboshop.cart.redis_host", value = "redis.devops2406.xyz", type = "String" },
+   { name = "roboshop.cart.catalogue_host", value = "catalogue.devops2406.xyz", type = "String" },
+   { name = "roboshop.cart.catalogue_port", value = "8080", type = "String" },
+   { name = "roboshop.catalogue.mongo", value = "true", type = "String" },
+   { name = "roboshop.catalogue.mongo_url", value = "mongodb:/mongodb.devops2406.xyz:27017/catalogue", type = "String" },
+   { name = "roboshop.payment.cart_host", value = "cart.devops2406.xyz", type = "String" },
+   { name = "roboshop.payment.cart_host", value = "8080", type = "String" },
+   { name = "roboshop.payment.user_host", value = "user.devops2406.xyz", type = "String" },
+   { name = "roboshop.payment.user_port", value = "8080", type = "String" },
+   { name = "roboshop.payment.amqp_host", value = "rabbitmq.devops2406.xyz", type = "String" },
+   { name = "roboshop.payment.amqp_user", value = "roboshop", type = "String" },
+   { name = "roboshop.payment.ampq_pass", value = "roboshop123", type = "SecureString" },
+   { name = "roboshop.shipping.cart_endpoint", value = "cart.devops2406.xyz", type = "String" },
+   { name = "roboshop.shipping.db_host", value = "mysql.devops2406.xyz", type = "String" },
+   { name = "roboshop.user.mongo", value = "true", type = "String" },
+   { name = "roboshop.user.redis_host", value = "redis.devops2406.xyz", type = "String" },
+   { name = "roboshop.user.mongo_url", value = "mongodb:/mongodb.devops2406.xyz:27017/users", type = "String" },
+
+
+   { name = "roboshop.payment.ampq_pass", value = "roboshop123", type = "SecureString" },
+
+ ]
 }
