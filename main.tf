51ebd7bc1a5809eebf02ae45b72fb8bde7c7c53f
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-08-10 11:53:46 +0530

    terraform

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ variable "params"{
    { name = "roboshop.cart.catalogue_host", value = "catalogue.devops2406.xyz", type = "String" },
    { name = "roboshop.cart.catalogue_port", value = "8080", type = "String" },
    { name = "roboshop.catalogue.mongo", value = "true", type = "String" },
-   { name = "roboshop.catalogue.mongo_url", value = "mongodb:/mongodb.devops2406.xyz:27017/catalogue", type = "String" },
+   { name = "roboshop.catalogue.mongo_url", value = "mongodb://mongodb.devops2406.xyz:27017/catalogue", type = "String" },
    { name = "roboshop.payment.cart_host", value = "cart.devops2406.xyz", type = "String" },
    { name = "roboshop.payment.cart_port", value = "8080", type = "String" },
    { name = "roboshop.payment.user_host", value = "user.devops2406.xyz", type = "String" },
@@ -27,7 +27,7 @@ variable "params"{
    { name = "roboshop.shipping.db_host", value = "mysql.devops2406.xyz", type = "String" },
    { name = "roboshop.user.mongo", value = "true", type = "String" },
    { name = "roboshop.user.redis_host", value = "redis.devops2406.xyz", type = "String" },
-   { name = "roboshop.user.mongo_url", value = "mongodb:/mongodb.devops2406.xyz", type = "String" },
+   { name = "roboshop.user.mongo_url", value = "mongodb://mongodb.devops2406.xyz:27017/users", type = "String" },
    { name = "roboshop.dispatch.amqp_host", value = "rabbitmq.devops2406.xyz", type = "String" },
    { name = "roboshop.dispatch.amqp_user", value = "roboshop", type = "String" },
 
