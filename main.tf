a5e2395e901d9eefbf187d62e7a44b401ce497af
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-08-11 10:18:57 +0530

    terraform

--- main.tf
+++ main.tf
@@ -7,11 +7,11 @@ resource "aws_ssm_parameter" "params" {
 
 variable "params"{
  default = [
-   { name = "roboshop.frontend.catalogue_url", value = "http://catalogue-dev.devops2406.xyz:8080/", type = "String" },
-   { name = "roboshop.frontend.user_url", value = "http://user-dev.devops2406.xyz:8080/", type = "String" },
-   { name = "roboshop.frontend.cart_url", value = "http://cart-dev.devops2406.xyz8080/", type = "String" },
-   { name = "roboshop.frontend.payment_url", value = "http://payment-dev.devops2406.xyz:8080/", type = "String" },
-   { name = "roboshop.frontend.shipping_url", value = "http://shipping-dev.devops2406.xyz:8080/", type = "String" },
+   { name = "roboshop.dev.frontend.catalogue_url", value = "http://catalogue-dev.devops2406.xyz:8080/", type = "String" },
+   { name = "roboshop.dev.frontend.user_url", value = "http://user-dev.devops2406.xyz:8080/", type = "String" },
+   { name = "roboshop.dev.frontend.cart_url", value = "http://cart-dev.devops2406.xyz8080/", type = "String" },
+   { name = "roboshop.dev.frontend.payment_url", value = "http://payment-dev.devops2406.xyz:8080/", type = "String" },
+   { name = "roboshop.dev.frontend.shipping_url", value = "http://shipping-dev.devops2406.xyz:8080/", type = "String" },
 
  ]
 }
