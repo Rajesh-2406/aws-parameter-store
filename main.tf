a0111adf1ac4271fead86961bbf218f0c7d9cb62
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-08-14 09:02:39 +0530

    terraform

--- main.tf
+++ main.tf
@@ -7,11 +7,11 @@ resource "aws_ssm_parameter" "params" {
 
 variable "params"{
  default = [
-   { name = "roboshop.frontend.catalogue_url", value = "http://catalogue-dev.devops2406.xyz", type = "String" },
-   { name = "roboshop.frontend.user_url", value = "http://user-dev.devops2406.xyz", type = "String" },
-   { name = "roboshop.frontend.cart_url", value = "http://cart-dev.devops2406.xyz", type = "String" },
-   { name = "roboshop.frontend.payment_url", value = "http://payment-dev.devops2406.xyz", type = "String" },
-   { name = "roboshop.frontend.shipping_url", value = "http://shipping-dev.devops2406.xyz", type = "String" },
+   { name = "roboshop.frontend.catalogue_url", value = "http://catalogue-dev.devops2406.xyz:8080/", type = "String" },
+   { name = "roboshop.frontend.user_url", value = "http://user-dev.devops2406.xyz:8080/", type = "String" },
+   { name = "roboshop.frontend.cart_url", value = "http://cart-dev.devops2406.xyz:8080/", type = "String" },
+   { name = "roboshop.frontend.payment_url", value = "http://payment-dev.devops2406.xyz:8080", type = "String" },
+   { name = "roboshop.frontend.shipping_url", value = "http://shipping-dev.devops2406.xyz:8080/", type = "String" },
    { name = "roboshop.cart.redis_host", value = "http://redis-dev.devops2406.xyz", type = "String" },
    { name = "roboshop.cart.catalogue_host", value = "http://catalogue-dev.devops2406.xyz", type = "String" },
    { name = "roboshop.cart.catalogue_port", value = "8080", type = "String" },
