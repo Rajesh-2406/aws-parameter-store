4d6a3c7e3613c3b6f0f960b499e665610cce9f93
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-09-30 10:19:39 +0530

    terraform

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ variable "params"{
    { name = "roboshop.dev.frontend.cart_url", value = "http://cart-dev.devops2406.xyz/", type = "String" },
    { name = "roboshop.dev.frontend.payment_url", value = "http://payment-dev.devops2406.xyz/", type = "String" },
    { name = "roboshop.dev.frontend.shipping_url", value = "http://shipping-dev.devops2406.xyz/", type = "String" },
-   { name = "roboshop.dev.cart.redis_host", value = "elasticache-dev.vxqnh7.clustercfg.use1.cache.amazonaws.com", type = "String" },
+   { name = "roboshop.dev.cart.redis_host", value = "elasticache-dev.ajrcky.clustercfg.use1.cache.amazonaws.com:6379", type = "String" },
    { name = "roboshop.dev.cart.catalogue_host", value = "catalogue-dev.devops2406.xyz", type = "String" },
    { name = "roboshop.dev.cart.catalogue_port", value = "80", type = "String" },
 
@@ -31,14 +31,14 @@ variable "params"{
    { name = "roboshop.dev.shipping.cart_endpoint", value = "cart-dev.devops2406.xyz", type = "String" },
    { name = "roboshop.dev.shipping.db_host", value = "mysql-dev.devops2406.xyz", type = "String" },
    { name = "roboshop.dev.user.mongo", value = "true", type = "String" },
-   { name = "roboshop.dev.user.redis_host", value = "elasticache-dev.vxqnh7.clustercfg.use1.cache.amazonaws.com", type = "String"},
+   { name = "roboshop.dev.user.redis_host", value = "elasticache-dev.ajrcky.clustercfg.use1.cache.amazonaws.com:6379", type = "String"},
 /*
    { name = "roboshop.dev.user.mongo_url", value = "mongodb://mongodb-dev.devops2406.xyz/users", type = "String" },
 */
    { name = "roboshop.dev.docdb.username", value = "roboshop", type = "String" },
    { name = "roboshop.dev.rabbitmq.amqp_user", value = "roboshop", type = "String" },
    { name = "roboshop.dev.mysql.username", value = "roboshop", type = "String" },
-   { name = "roboshop.dev.mysql.endpoint", value = "rds-dev.cluster-c2cprqmiz9av.us-east-1.rds.amazonaws.com", type = "String" },
+   { name = "roboshop.dev.mysql.endpoint", value = "rds-dev.cluster-crpaxg4qag9m.us-east-1.rds.amazonaws.com", type = "String" },
 
 
 
@@ -50,7 +50,6 @@ variable "params"{
    { name = "roboshop.dev.docdb.password", value = "roboshop123", type = "SecureString" },
    { name = "roboshop.dev.catalogue.mongo_url", value = "mongodb://roboshop:roboshop123@docdb-dev-0.c2cprqmiz9av.us-east-1.docdb.amazonaws.com:27017/catalogue?tls=true&tlsCAFile=global-bundle.pem&retryWrites=false", type = "SecureString" },
    { name = "roboshop.dev.catalogue.docdb_endpoint",value ="mongo --ssl --host docdb-dev.cluster-crpaxg4qag9m.us-east-1.docdb.amazonaws.com", type = "String" },
-
    { name = "roboshop.dev.user.docdb_endpoint",value ="docdb-dev.cluster-c2cprqmiz9av.us-east-1.docdb.amazonaws.com", type = "String" },
    { name = "roboshop.dev.user.mongo_url", value = "mongodb://roboshop:roboshop123@docdb-dev-0.c2cprqmiz9av.us-east-1.docdb.amazonaws.com:27017/users?tls=true&tlsCAFile=global-bundle.pem&retryWrites=false", type = "SecureString" },
 
@@ -63,7 +62,7 @@ variable "params"{
 { name = "roboshop.prod.frontend.cart_url", value = "http://cart-prod.devops2406.xyz/", type = "String" },
 { name = "roboshop.prod.frontend.payment_url", value = "http://payment-prod.devops2406.xyz/", type = "String" },
 { name = "roboshop.prod.frontend.shipping_url", value = "http://shipping-prod.devops2406.xyz/", type = "String" },
-{ name = "roboshop.prod.cart.redis_host", value = "elasticache-prod.vxqnh7.clustercfg.use1.cache.amazonaws.com", type = "String" },
+{ name = "roboshop.prod.cart.redis_host", value = "eelasticache-prod.ajrcky.clustercfg.use1.cache.amazonaws.com:6379", type = "String" },
 { name = "roboshop.prod.cart.catalogue_host", value = "catalogue-prod.devops2406.xyz", type = "String" },
 { name = "roboshop.prod.cart.catalogue_port", value = "80", type = "String" },
 
@@ -80,14 +79,14 @@ variable "params"{
 { name = "roboshop.prod.shipping.cart_endpoint", value = "cart-prod.devops2406.xyz", type = "String" },
 { name = "roboshop.prod.shipping.db_host", value = "mysql-prod.devops2406.xyz", type = "String" },
 { name = "roboshop.prod.user.mongo", value = "true", type = "String" },
-{ name = "roboshop.prod.user.redis_host", value = "elasticache-prod.vxqnh7.clustercfg.use1.cache.amazonaws.com", type = "String"},
+{ name = "roboshop.prod.user.redis_host", value = "elasticache-prod.ajrcky.clustercfg.use1.cache.amazonaws.com:6379", type = "String"},
 /*
    { name = "roboshop.dev.user.mongo_url", value = "mongodb://mongodb-dev.devops2406.xyz/users", type = "String" },
 */
 { name = "roboshop.prod.docdb.username", value = "roboshop", type = "String" },
 { name = "roboshop.prod.rabbitmq.amqp_user", value = "roboshop", type = "String" },
 { name = "roboshop.prod.mysql.username", value = "roboshop", type = "String" },
-{ name = "roboshop.prod.mysql.endpoint", value = "rds-prod.cluster-c2cprqmiz9av.us-east-1.rds.amazonaws.com", type = "String" },
+{ name = "roboshop.prod.mysql.endpoint", value = "rds-prod.cluster-crpaxg4qag9m.us-east-1.rds.amazonaws.com", type = "String" },
 
 
 
@@ -98,9 +97,9 @@ variable "params"{
 { name = "roboshop.prod.mysql.password", value = "roboshop123", type = "SecureString" },
 { name = "roboshop.prod.docdb.password", value = "roboshop123", type = "SecureString" },
 { name = "roboshop.prod.catalogue.mongo_url", value = "mongodb://roboshop:roboshop123@docdb-prod-0.c2cprqmiz9av.us-east-1.docdb.amazonaws.com:27017/catalogue?tls=true&tlsCAFile=global-bundle.pem&retryWrites=false", type = "SecureString" },
-{ name = "roboshop.prod.catalogue.docdb_endpoint",value ="docdb-prod.cluster-c2cprqmiz9av.us-east-1.docdb.amazonaws.com", type = "String" },
+{ name = "roboshop.prod.catalogue.docdb_endpoint",value ="mongo --ssl --host docdb-prod.cluster-crpaxg4qag9m.us-east-1.docdb.amazonaws.com", type = "String" },
 
-{ name = "roboshop.prod.user.docdb_endpoint",value ="docdb-prod.cluster-c2cprqmiz9av.us-east-1.docdb.amazonaws.com", type = "String" },
+{ name = "roboshop.prod.user.docdb_endpoint",value ="docdb-prod.cluster-crpaxg4qag9m.us-east-1.docdb.amazonaws.com", type = "String" },
 { name = "roboshop.prod.user.mongo_url", value = "mongodb://roboshop:roboshop123@docdb-prod-0.c2cprqmiz9av.us-east-1.docdb.amazonaws.com:27017/users?tls=true&tlsCAFile=global-bundle.pem&retryWrites=false", type = "SecureString" },
 
 ]
